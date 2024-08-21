30d2b52a611092616e5f31eb113ab1383ab47d90
Jahaziel0311 <62921040+Jahaziel0311@users.noreply.github.com>
2024-08-21 10:01:11 -0500

    dynamodb

---
+++ app/Models/DynamoDB/paciente.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\DynamoDB;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class paciente extends Model
+{
+    use HasFactory;
+}
--- config/database.php
+++ config/database.php
@@ -90,6 +90,13 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
+        'dynamodb' => [
+            'driver' => 'dynamodb',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION', 'us-east-2'),
+        ],
+
 
     ],
 
